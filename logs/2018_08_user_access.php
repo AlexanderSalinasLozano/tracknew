658dfe06e8c71caa67f0255870d053325f5b3403
Alexander-Salinas <55986618+AlexanderSalinas-Stech@users.noreply.github.com>
2023-02-09 12:55:28 -0300

    Delete logs directory

--- logs/2018_08_user_access.php
+++
@@ -1,9 +0,0 @@
-<?
-session_start();
-include ('../init.php');
-include ('../func/fn_common.php');
-checkUserSession();
-checkUserCPanelPrivileges();
-header('Content-Type:text/plain');
-?>
-[2018-08-30 12:47:22] 190.153.243.116 [1]admin - User login: successful
